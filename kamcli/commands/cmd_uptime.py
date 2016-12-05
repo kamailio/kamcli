7360965ecda9b96cc82a29a6b6c9f758e2829398
Daniel-Constantin Mierla <miconda@gmail.com>
2016-12-05 22:41:26 +0100

    added uptime command

---
+++ kamcli/commands/cmd_uptime.py
@@ -0,0 +1,17 @@
+import click
+from kamcli.cli import pass_context
+from kamcli.iorpc import command_ctl
+
+
+##
+#
+#
+@click.command('uptime', short_help='Print the uptime for kamailio')
+@pass_context
+def cli(ctx):
+    """Print the uptime for kamailio
+
+    \b
+    Show time details since kamailio was started.
+    """
+    command_ctl(ctx, 'core.uptime', [])
