e96b92e3167f2b4ed3ea92f2206c56c9f315ad8f
Daniel-Constantin Mierla <miconda@gmail.com>
2019-12-11 10:35:37 +0100

    cmd_acc: separate commands to update acc or missed_calls tables structures

--- kamcli/commands/cmd_acc.py
+++ kamcli/commands/cmd_acc.py
@@ -12,16 +12,7 @@ def cli(ctx):
     pass
 
 
-@cli.command(
-    "acc-struct-update",
-    help="Run SQL statements to update acc table structure",
-)
-@pass_context
-def acc_struct_update(ctx):
-    """Run SQL statements to update acc table structure
-    """
-    ctx.vlog("Run statements to update acc and missed_calls tables structures")
-    e = create_engine(ctx.gconfig.get("db", "rwurl"))
+def acc_acc_struct_update_exec(ctx, e):
     sqltext = """
       ALTER TABLE acc ADD COLUMN src_user VARCHAR(64) NOT NULL DEFAULT '';
       ALTER TABLE acc ADD COLUMN src_domain VARCHAR(128) NOT NULL DEFAULT '';
@@ -29,6 +20,25 @@ def acc_struct_update(ctx):
       ALTER TABLE acc ADD COLUMN dst_ouser VARCHAR(64) NOT NULL DEFAULT '';
       ALTER TABLE acc ADD COLUMN dst_user VARCHAR(64) NOT NULL DEFAULT '';
       ALTER TABLE acc ADD COLUMN dst_domain VARCHAR(128) NOT NULL DEFAULT '';
+    """
+    dbutils_exec_sqltext(ctx, e, sqltext)
+
+
+@cli.command(
+    "acc-struct-update",
+    help="Run SQL statements to update acc table structure",
+)
+@pass_context
+def acc_acc_struct_update(ctx):
+    """Run SQL statements to update acc table structure
+    """
+    ctx.vlog("Run statements to update acc table structure")
+    e = create_engine(ctx.gconfig.get("db", "rwurl"))
+    acc_acc_struct_update_exec(ctx, e)
+
+
+def acc_mc_struct_update_exec(ctx, e):
+    sqltext = """
       ALTER TABLE missed_calls ADD COLUMN src_user VARCHAR(64) NOT NULL DEFAULT '';
       ALTER TABLE missed_calls ADD COLUMN src_domain VARCHAR(128) NOT NULL DEFAULT '';
       ALTER TABLE missed_calls ADD COLUMN src_ip varchar(64) NOT NULL default '';
@@ -37,3 +47,30 @@ def acc_struct_update(ctx):
       ALTER TABLE missed_calls ADD COLUMN dst_domain VARCHAR(128) NOT NULL DEFAULT '';
     """
     dbutils_exec_sqltext(ctx, e, sqltext)
+
+
+@cli.command(
+    "mc-struct-update",
+    help="Run SQL statements to update missed_calls table structure",
+)
+@pass_context
+def acc_mc_struct_update(ctx):
+    """Run SQL statements to update missed_calls table structure
+    """
+    ctx.vlog("Run statements to update missed_calls table structure")
+    e = create_engine(ctx.gconfig.get("db", "rwurl"))
+    acc_mc_struct_update_exec(ctx, e)
+
+
+@cli.command(
+    "tables-struct-update",
+    help="Run SQL statements to update acc and missed_calls tables structures",
+)
+@pass_context
+def acc_tables_struct_update(ctx):
+    """Run SQL statements to update acc and missed_calls tables structures
+    """
+    ctx.vlog("Run statements to update acc and missed_calls tables structures")
+    e = create_engine(ctx.gconfig.get("db", "rwurl"))
+    acc_acc_struct_update_exec(ctx, e)
+    acc_mc_struct_update_exec(ctx, e)
