07f4b6a70839eb386bcbb9172986575b73f43739
Daniel-Constantin Mierla <miconda@gmail.com>
2020-08-09 19:27:27 +0200

    cmd_dlgs: added briefieng and getall subcommands

--- kamcli/commands/cmd_dlgs.py
+++ kamcli/commands/cmd_dlgs.py
@@ -20,13 +20,25 @@ def dlgs_list(ctx):
     command_ctl(ctx, "dlgs.list", [])
 
 
-@cli.command("get", short_help="Get the details for the matching dialogs")
+@cli.command(
+    "briefing", short_help="Show summary for dialog records in memory"
+)
+@pass_context
+def dlgs_briefing(ctx):
+    """Show summary for dialog records in memory
+
+    \b
+    """
+    command_ctl(ctx, "dlgs.briefing", [])
+
+
+@cli.command("get", short_help="Get the details for the first matching dialog")
 @click.argument("mkey", metavar="<mkey>")
 @click.argument("mop", metavar="<mkey>")
 @click.argument("mval", metavar="<mval>")
 @pass_context
 def dlgs_get(ctx, mkey, mop, mval):
-    """Show the details for the matching dialogs
+    """Show the details for the first matching dialog
 
     \b
     Parameters:
@@ -43,3 +55,28 @@ def dlgs_get(ctx, mkey, mop, mval):
         <mval> - matching value
     """
     command_ctl(ctx, "dlgs.get", [mkey, mop, mval])
+
+
+@cli.command("getall", short_help="Get the details for all matching dialogs")
+@click.argument("mkey", metavar="<mkey>")
+@click.argument("mop", metavar="<mkey>")
+@click.argument("mval", metavar="<mval>")
+@pass_context
+def dlgs_getall(ctx, mkey, mop, mval):
+    """Show the details for all matching dialogs
+
+    \b
+    Parameters:
+        <mkey> - matching key:
+          src - src attribute;
+          dst - dst attribute;
+          data - data attribute;
+        <mop> - matching operator:
+          eq - equal string comparison;
+          ne - not-equal string comparison;
+          re - regular expression;
+          sw - starts-with
+          fm - fast match
+        <mval> - matching value
+    """
+    command_ctl(ctx, "dlgs.getall", [mkey, mop, mval])
