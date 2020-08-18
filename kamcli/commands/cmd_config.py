d0f6707edc4391cbcfe6cc5d2714a0e913a3242d
Daniel-Constantin Mierla <miconda@gmail.com>
2020-08-18 15:25:18 +0200

    cmd_config: option to install config in user directory

--- kamcli/commands/cmd_config.py
+++ kamcli/commands/cmd_config.py
@@ -56,16 +56,22 @@ def config_cmdaliases(ctx):
 
 
 @cli.command("install", short_help="Install the config file")
+@click.option(
+    "user", "--user", "-u", is_flag=True, help="Install in user home folder",
+)
 @pass_context
-def config_install(ctx):
+def config_install(ctx, user):
     if os.path.isfile("./kamcli/kamcli.ini"):
-        dirName = "/etc/kamcli"
+        if user:
+            dirName = os.path.expanduser("~/.kamcli")
+        else:
+            dirName = "/etc/kamcli"
         if not os.path.exists(dirName):
             os.mkdir(dirName)
             click.echo("directory " + dirName + " created")
         else:
             click.echo("directory " + dirName + " already exists")
-        shutil.copyfile("./kamcli/kamcli.ini", "/etc/kamcli/kamcli.ini")
-        click.echo("config file installed to /etc/kamcli/kamcli.ini")
+        shutil.copyfile("./kamcli/kamcli.ini", dirName + "/kamcli.ini")
+        click.echo("config file installed to " + dirName + "/kamcli.ini")
     else:
         click.echo("command must be run in the source code root directory")
