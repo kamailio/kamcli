fc8e7c84d3660f1d371f5807ae4b5ab61d02f047
Daniel-Constantin Mierla <miconda@gmail.com>
2020-10-18 10:29:56 +0200

    cmd_pipelimit: added db add and show commands

--- kamcli/commands/cmd_pipelimit.py
+++ kamcli/commands/cmd_pipelimit.py
@@ -1,4 +1,6 @@
 import click
+from sqlalchemy import create_engine
+from kamcli.ioutils import ioutils_dbres_print
 from kamcli.cli import pass_context
 from kamcli.iorpc import command_ctl
 
@@ -11,6 +13,102 @@ from kamcli.iorpc import command_ctl
 @pass_context
 def cli(ctx):
     pass
+
+
+@cli.command("db-add", short_help="Add a new pipelimit record to database")
+@click.option(
+    "dbtname",
+    "--dbtname",
+    default="pl_pipes",
+    help='The name of database table (default: "pl_pipes")',
+)
+@click.option(
+    "alg",
+    "--alg",
+    default="taildrop",
+    help='The name of the algorithm (default: "taildrop")',
+)
+@click.argument("pipeid", metavar="<pipeid>")
+@click.argument("limit", metavar="<limit>", type=click.INT)
+@pass_context
+def pipelimit_dbadd(ctx, dbtname, alg, pipeid, limit):
+    """Add a new pipelimit record in database table
+
+    \b
+    Parameters:
+        <pipeid> - pipe name id
+        <limit> - pipe limit
+    """
+    ctx.vlog(
+        "Adding to pipelimit table [%s] record: [%s] [%s] [%s]",
+        dbtname,
+        pipeid,
+        limit,
+        alg,
+    )
+    e = create_engine(ctx.gconfig.get("db", "rwurl"))
+    dbtval = dbtname.encode("ascii", "ignore").decode()
+    pidval = pipeid.encode("ascii", "ignore").decode()
+    algval = alg.encode("ascii", "ignore").decode()
+    e.execute(
+        "insert into {0} (pipeid, algorithm, plimit) values ({1!r}, {2!r}, {3})".format(
+            dbtval, pidval, algval, limit
+        )
+    )
+
+
+@cli.command("db-show", short_help="Show pipelimit records in database")
+@click.option(
+    "oformat",
+    "--output-format",
+    "-F",
+    type=click.Choice(["raw", "json", "table", "dict"]),
+    default=None,
+    help="Format the output",
+)
+@click.option(
+    "ostyle",
+    "--output-style",
+    "-S",
+    default=None,
+    help="Style of the output (tabulate table format)",
+)
+@click.option(
+    "dbtname",
+    "--dbtname",
+    default="pl_pipes",
+    help='The name of database table (default: "pl_pipes")',
+)
+@click.argument("pipeid", nargs=-1, metavar="[<pipeid>]")
+@pass_context
+def pipelimit_dbshow(ctx, oformat, ostyle, dbtname, pipeid):
+    """Show details for records in pipelimit database table
+
+    \b
+    Parameters:
+        <pipeid> - pipe name id (optional)
+    """
+    e = create_engine(ctx.gconfig.get("db", "rwurl"))
+    if not pipeid:
+        ctx.vlog("Showing all pipelimit database records")
+        res = e.execute(
+            "select * from {0}".format(
+                dbtname.encode("ascii", "ignore").decode()
+            )
+        )
+        ioutils_dbres_print(ctx, oformat, ostyle, res)
+    else:
+        for p in pipeid:
+            ctx.vlog(
+                "Showing pipelimit database records for pipeid {0}".format(p)
+            )
+            res = e.execute(
+                "select * from {0} where pipeid={1!r}".format(
+                    dbtname.encode("ascii", "ignore").decode(),
+                    p.encode("ascii", "ignore").decode(),
+                )
+            )
+            ioutils_dbres_print(ctx, oformat, ostyle, res)
 
 
 @cli.command("list", short_help="Lists the details of one or all pipes")
