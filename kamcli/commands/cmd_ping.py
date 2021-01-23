94b092be9bf180681658797e208baa52f69a8eaa
Daniel-Constantin Mierla <miconda@gmail.com>
2021-01-23 19:40:27 +0100

    cmd_ping: added option to set from uri

--- kamcli/commands/cmd_ping.py
+++ kamcli/commands/cmd_ping.py
@@ -5,27 +5,33 @@ from kamcli.iorpc import command_ctl
 
 
 @click.command("ping", short_help="Send an OPTIONS ping request")
+@click.option("furi", "--furi", default="", help='From URI (default: "")')
 @click.argument("uri", nargs=1, metavar="[<uri>]")
 @pass_context
-def cli(ctx, uri):
+def cli(ctx, furi, uri):
     """Send an OPTIONS ping request
 
         Parameters:
             - <uri> - the SIP URI of the target
     \b
     """
-    ldomain = ctx.gconfig.get("main", "domain", fallback="localhost")
+    lfrom = ""
+    if len(furi) == 0:
+        ldomain = ctx.gconfig.get("main", "domain", fallback="localhost")
+        lfrom = "sip:daemon@" + ldomain
+    else:
+        lfrom = furi
     plist = [
         "OPTIONS",
         uri,
         ".",
         ".",
-        "From: <sip:daemon@"
-        + ldomain
+        "From: <"
+        + lfrom
         + ">\r\nTo: <"
         + uri
-        + ">\r\n Contact:<sip:daemon@"
-        + ldomain
+        + ">\r\n Contact: <"
+        + lfrom
         + ">\r\n",
     ]
     command_ctl(ctx, "tm.t_uac_wait_block", plist)
