974b2147e25895661f742889750064cbccacf8b7
Daniel-Constantin Mierla <miconda@gmail.com>
2021-01-28 09:31:37 +0100

    cmd_sipreq: added option to set additional headers

--- kamcli/commands/cmd_sipreq.py
+++ kamcli/commands/cmd_sipreq.py
@@ -25,6 +25,9 @@ from kamcli.iorpc import command_ctl
     "duri", "--duri", "-d", default=".", help='Destination URI (default: "")'
 )
 @click.option(
+    "hdrs", "--hdrs", "-a", default="", help='Additional headers (default: "")'
+)
+@click.option(
     "socket",
     "--socket",
     "-s",
@@ -36,11 +39,17 @@ from kamcli.iorpc import command_ctl
 )
 @click.argument("uri", nargs=1, metavar="[<uri>]")
 @pass_context
-def cli(ctx, nowait, method, furi, curi, duri, socket, body, uri):
+def cli(ctx, nowait, method, furi, curi, duri, hdrs, socket, body, uri):
     """Send a SIP request via RPC command
 
         Parameters:
             - <uri> - the SIP URI of the target
+
+        Note: additional headers must not include From, To or Contact
+        headers, these are generated from the other parameters. If From
+        URI is not provided, then it is generated using local host. The
+        To URI is set to <uri>. If Contact URI is not provided, then it
+        is set to From URI.
     \b
     """
     lcmd = "tm.t_uac_wait_block"
@@ -55,18 +64,25 @@ def cli(ctx, nowait, method, furi, curi, duri, socket, body, uri):
     lcuri = lfrom
     if len(curi) != 0:
         lcuri = curi
-    plist = [
-        method,
-        uri,
-        duri,
-        socket,
+    lhdrs = (
         "From: <"
         + lfrom
         + ">\r\nTo: <"
         + uri
         + ">\r\n Contact: <"
         + lcuri
-        + ">\r\n",
+        + ">\r\n"
+    )
+    if len(hdrs) != 0:
+        lhdrs += hdrs
+        if lhdrs[-2:] != "\r\n":
+            lhdrs += "\r\n"
+    plist = [
+        method,
+        uri,
+        duri,
+        socket,
+        lhdrs,
         body,
     ]
     command_ctl(ctx, lcmd, plist)
